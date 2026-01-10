18a8981cd7d69df4b46f3c391d32532cfe9782c0
ntalbs <ntalbsen@gmail.com>
2026-01-10 15:58:09 +0000

    Add CLI options

--- README.md
+++ README.md
@@ -2,4 +2,32 @@
 
 `hanfix`는 [다음 맞춤법
 검사기](https://dic.daum.net/grammar_checker.do)를 이용한 한글 맞춤법
-검사기입니다.
+검사기입니다. [hanspell](https://github.com/9beach/hanspell)에서
+영감을 받아 작성했습니다. `hanspell`과 다른 점은 다음과 같습니다.
+
+- 맞춤법 검사는 다음 맞춤법 검사기만 사용합니다.
+- 입력 텍스트를 `-d`(또는 `--data`) 옵션으로 제공할 수 있습니다. 이
+  옵션을 지정하지 않은 경우에는 STDIN으로부터 입력을 기다립니다.
+
+## 설치
+
+`hanfix`는 다음과 같이 설치할 수 있습니다.
+
+```console
+npm install -g hanfix
+```
+
+
+## 사용법
+
+```console
+$ hanfix --help
+Usage: index [options]
+
+Korean Spell Checker
+
+Options:
+  -d, --data <text>        text for spell check
+  -c, --include-corrected  include corrected text in the result
+  -h, --help               display help for command
+```
--- src/index.js
+++ src/index.js
@@ -1,26 +1,58 @@
 #!/usr/bin/env node
 
 import fs from 'fs';
+import { program } from 'commander';
 import { check } from './hanfix.js';
 
-try {
-  const inputText = fs.readFileSync(0, 'utf8');
-  const result = await check(inputText);
+program
+  .description('Korean Spell Checker')
+  .option('-d, --data <text>', 'text for spell check')
+  .option('-c, --include-corrected', 'include corrected text in the result')
+  .action((options) => {
+    if (!options.data) {
+      readFromStdin(options);
+    } else {
+      checkText(options.data, options);
+    }
+  })
+  .parse(process.argv);
 
+async function readFromStdin (options) {
+  try {
+    const inputText = fs.readFileSync(0, 'utf8');
+    const result = await check(inputText);
+    const output = {
+      errors: result
+    };
+
+    if (options.includeCorrected) {
+      output.corrected = correctText(inputText, result);
+    }
+
+    process.stdout.write(JSON.stringify(output));
+  } catch (err) {
+    process.stderr.write(err.message);
+    process.exit(1);
+  }
+}
+
+async function checkText(inputText, options) {
+  const result = await check(inputText);
+  const output = {
+    errors: result
+  };
+
+  if (options.includeCorrected) {
+    output.corrected = correctText(inputText, result);
+  }
+
+  process.stdout.write(JSON.stringify(output));
+}
+
+function correctText(inputText, result) {
   let correctedText = inputText;
   for (const e of result) {
     correctedText = correctedText.replaceAll(e.input, e.output);
   }
-
-  const output = {
-    errors: result,
-    corrected: correctedText
-  }
-
-//  process.stdout.write(JSON.stringify(output));
-  console.log(output)
-
-} catch (err) {
-  process.stderr.write(err.message);
-  process.exit(1);
+  return correctedText;
 }
